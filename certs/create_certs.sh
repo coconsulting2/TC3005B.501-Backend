f58fb8608ffbd0708fc8a22fc16d6b7f25095c28
luisda25 <luisdaf04@outlook.com>
2025-04-07 23:44:26 -0600

    fix(https certificates): fixed certificates creation script to create correctly all certificates

--- certs/create_certs.sh
+++ certs/create_certs.sh
@@ -7,10 +7,10 @@ openssl req -x509 -new -nodes -key ca.key -sha256 -days 365 -out ca.crt
 # Generate private key for the server 
 openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -out server.key
 
-# Create the Certificate Signing Request (CSR) 
-openssl req -new -key server.key -out server.csr
+# Create the Certificate Signing Request (CSR) and signing certificates using CA  
+openssl req -new -key server.key -out server.csr 
 
-# Sign Certificate using CA 
-openssl x509 -re -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 365 -sha256
+# Sign certificates using CA 
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 365 -sha256
 
 echo "Certificates generated succesfully"
