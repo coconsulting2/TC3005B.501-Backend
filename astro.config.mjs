ac5e9e9e554a3e0cd99dffd028584ebddef9ed86
RoSosa <a01027913@tec.mx>
2025-04-07 21:45:18 -0600

    feat: add configuration for https usage in vite/astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss(), basicSsl()],
+    server: {
+      https: true
+    }
   }
 });
