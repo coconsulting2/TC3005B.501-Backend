f391038513be81225417ede40cf12581fe63495d
A <A01027920@tec.mx>
2025-04-11 19:38:02 -0600

    added user and roles table

--- database/Schema/Scheme.sql
+++ database/Schema/Scheme.sql
@@ -1,6 +1,25 @@
 CREATE DATABASE IF NOT EXISTS CocoScheme;
 USE CocoScheme;
 
+CREATE TABLE IF NOT EXISTS Roles(
+    role_id INT PRIMARY KEY AUTO_INCREMENT,
+    role_name VARCHAR(20),
+);
+
+CREATE TABLE IF NOT EXISTS User(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    role_id INT,
+    user_name VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    workstation VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone_number VARCHAR(20) UNIQUE NOT NULL,
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_mod_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    active BOOLEAN,
+    FOREIGN KEY (role_id) REFERENCES roles(role_id)
+);
+
 CREATE TABLE Request (
     request_id INT PRIMARY KEY,
     status enum,
