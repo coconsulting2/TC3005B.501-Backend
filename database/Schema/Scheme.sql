0d29eb2be17309212e9680801ce89234ce2f60d8
natrogue28 <A01663909@tec.mx>
2025-04-11 19:24:17 -0600

    Feature(DB) Route table created

--- database/Schema/Scheme.sql
+++ database/Schema/Scheme.sql
@@ -15,3 +15,19 @@ CREATE TABLE Request (
     user_id INT,
     route_request_id INT
 );
+
+
+CREATE TABLE Route (
+    route_id INT PRIMARY KEY,
+    router_index INT,
+    id_origin_country VARCHAR(10),
+    id_origin_city VARCHAR(10),
+    id_destination_country VARCHAR(10),
+    id_destination_city VARCHAR(10),
+    plane_needed BOOL,
+    hotel_needed BOOL,
+    begining_date DATE,
+    begining_time TIME,
+    ending_date DATE,
+    ending_time TIME
+);
