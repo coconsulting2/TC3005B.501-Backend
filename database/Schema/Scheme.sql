bf1fb4940ff589472af733f88a6db4ec56630d77
a01029143 <a010291443@tec.mx>
2025-04-11 18:51:58 -0600

    Feature(DB) Request_Table created

--- database/Schema/Scheme.sql
+++ database/Schema/Scheme.sql
@@ -1,2 +1,17 @@
 CREATE DATABASE IF NOT EXISTS CocoScheme;
 USE CocoScheme;
+
+CREATE TABLE Request (
+    request_id INT PRIMARY KEY,
+    status enum,
+    notes losngtext,
+    requested_fee FLOAT,
+    imposed_fee FLOAT,
+    request_date DATE,
+    request_time TIME,
+    last_mod_date DATE,
+    last_mod_time TIME,
+    active BOOL,
+    user_id INT,
+    route_request_id INT
+);
