5727fcb093e2538a7ef5010127f62a6ae4850976
Valentina <valentina2212.gonzalez@gmail.com>
2025-04-14 14:07:10 -0600

    feat: updated controller

--- controllers/applicantController.js
+++ controllers/applicantController.js
@@ -20,7 +20,28 @@ const getApplicantById = async (req, res) => {
     }
 }
 
+const getCompletedRequests = async (req, res) => {
+    const id = req.params.id;
+    console.log(`Getting completed requests for id: ${id}`)
+    try {
+        const completedRequests = await Applicant.getCompletedRequests(id);
+        if (!completedRequests) {
+            return res.status(404).json({error: "No completed requests found for the user"});
+        }
+        const formattedRequests = completedRequests.map(request => ({
+            request_id: request.request_id,
+            destination_country: request.destination_country,
+            request_date: request.request_date,
+            status: request.status
+          }));
+        res.json(formattedRequests);
+    } catch(err) {
+        res.status(500).json({error: "CONTROLLER: Internal server error"});
+    }
+}
+
 export default {
     getApplicantById,
+    getCompletedRequests
     // other functions go here
 };
