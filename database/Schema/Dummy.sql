0b1fd0e898fd2c47df0be4b55b863f3324b4960d
a01029143 <a010291443@tec.mx>
2025-04-24 13:54:33 -0600

    Added 10 examples of dummy data for Country-Table

--- database/Schema/Dummy.sql
+++ database/Schema/Dummy.sql
@@ -1,1 +1,13 @@
 USE CocoScheme;
+
+INSERT INTO Country (receipt_type_name) VALUES
+    ('España'),
+    ('México'),
+    ('Estados Únidos'),
+    ('Alemania'),
+    ('Italia'),
+    ('Francia'),
+    ('Madagascar'),
+    ('El reino de nunca jamás de la estrella más alta a la derecha'),
+    ('El país de las maravillas'),
+    ('Skibidi Island Tralalero Tralala');
