10056ca488e166ef46f719263cf02c3caf47472f
a01029143 <a010291443@tec.mx>
2025-04-24 14:19:10 -0600

    Added 10 examples of dummy data for City Table

--- database/Schema/Dummy.sql
+++ database/Schema/Dummy.sql
@@ -1,1 +1,13 @@
 USE CocoScheme;
+
+INSERT INTO City (city_name) VALUES
+    ('CDMX'),
+    ('Guadalajara'),
+    ('Madrid'),
+    ('Barceloa'),
+    ('Tokyo'),
+    ('Kyoto'),
+    ('Nuke Town'),
+    ('Reino de Simba'),
+    ('Pisos Picados'),
+    ('Bombardino Cocodrilo');
