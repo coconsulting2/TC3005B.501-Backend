3a7b11a4c7a3c7bb1a4a1f53c8f54ad614c683f0
natrogue28 <A01663909@tec.mx>
2025-04-24 18:43:06 -0600

    Added dummy data

--- database/Schema/Dummy.sql
+++ database/Schema/Dummy.sql
@@ -24,3 +24,14 @@ INSERT INTO City (city_name) VALUES
     ('Pisos Picados'),
     ('Bombardino Cocodrilo');
 
+INSERT INTO Alert (user_id, alert_text, alert_date) VALUES
+    (1, 'Your password will expire in 3 days.', '2025-04-20 08:00:00'),
+    (2, 'XXXXXXXXXXXXXXXXXXX', '2025-04-20 09:00:00'),
+    (3, 'XXXXXXXXXXXXXXXXXXXX', '1850-04-15 10:00:00'),
+    (4, 'New login from an unknown device detected.', '1993-04-21 11:15:00'),
+    (5, '', '2025-04-21 12:00:000'),
+    (5, 'Lorem ipsumjjj.', '2025-04-18 11:33:27'),
+    (7, 'A very long alert message.  11?', '2025-04-22 08:45:00'),
+    (1, 'System maintenance scheduled at midnight. ffff', '2025-09-23 18:00:00'),
+    (9, 'Error processing your last request, please try again.', '2025-04-23 19:20:00'),
+    (10, 'Backup completed successfully.', '2020-18-24 06:10:00');
