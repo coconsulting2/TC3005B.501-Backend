47c8b885cf60bb42b83d46e5b2f5eb4f45aad6d8
A <A01027920@tec.mx>
2025-04-30 11:52:56 -0600

    fix(db): fixed the request_status_id that trigger the deletion of alert

--- database/Schema/Triggers.sql
+++ database/Schema/Triggers.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE TRIGGER DeactivateRequest
 BEFORE UPDATE ON `Request`
 FOR EACH ROW
 BEGIN
-    IF NEW.request_status_id IN (7, 8) THEN
+    IF NEW.request_status_id IN (9, 10) THEN
         SET NEW.active = FALSE;
     END IF;
 END$$
