bf76f21779f69362104107037924ab36a728994f
MESC <mesc2004@gmail.com>
2025-04-29 18:44:27 -0600

    fix: changed query values to work with db, changed rows to work.
    Changed names of values in the query of the `userController.js` file, as
    well as the table names from plural to singular using uppercase for the
    starting letter. Changed how `rows` is defined from `[rows]` to be able
    to properly recover the query parameters.

--- models/userModel.js
+++ models/userModel.js
@@ -8,21 +8,21 @@ import db from '../database/config/db.js';
 export async function getUserData(userId) {
   const connection = await db.getConnection();
   try {
-    const [rows] = await connection.query(
+    const rows = await connection.query(
       `SELECT 
         u.user_id, 
         u.user_name, 
         u.email, 
         u.phone_number, 
-        u.date_of_creation, 
+        u.creation_date, 
         r.role_name
-      FROM users u
-      JOIN roles r ON u.role_id = r.role_id
+      FROM User u
+      JOIN Role r ON u.role_id = r.role_id
       WHERE u.user_id = ?`,
       [userId]
     );
     
-    return rows.length ? rows[0] : null;
+    return rows[0];
   } finally {
     connection.release();
   }
