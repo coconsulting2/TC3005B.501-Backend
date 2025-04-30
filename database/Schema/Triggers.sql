020a1bcf7cc8ca5bc11e06c35a04c9468d3c8e73
A <A01027920@tec.mx>
2025-04-30 11:51:16 -0600

    fix(db): fixed request_status_id that trigger the trigger xd

--- database/Schema/Triggers.sql
+++ database/Schema/Triggers.sql
@@ -6,7 +6,7 @@ CREATE TRIGGER DeleteAlert
 AFTER UPDATE ON `Request`
 FOR EACH ROW
 BEGIN
-    IF NEW.request_status_id IN (6, 7, 8) THEN
+    IF NEW.request_status_id IN (8, 9, 10) THEN
         DELETE FROM Alert
         WHERE request_id = NEW.request_id;
     END IF;
