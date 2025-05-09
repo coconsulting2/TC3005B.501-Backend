1f41508ea0b6bbba44b34dee6edb0131c7d2c759
luisda25 <luisdaf04@outlook.com>
2025-05-09 13:06:52 -0600

    feat: add user route for login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 import * as userController from '../controllers/userController.js';
 
 router.get('/get-user-data/:user_id', userController.getUserData);
-
+router.post('/login', userController.login);
 
 export default router;
