f5ddfb1d6fd3da8d336ca3a2989be39c2734051a
luisda25 <luisdaf04@outlook.com>
2025-05-11 00:04:26 -0600

    refactor: move createUser model from user to admin

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,23 +1,60 @@
 /* 
 Admin Model
 */
-import pool from '../database/config/db.js';
-import { createUser as userModelCreateUser } from './userModel.js';
+import db from '../database/config/db.js';
 
 const Admin = {
   async createUser(userData) {
-    try {
+    const connection = await db.getConnection();
+    try{
+      const existingUser = await connection.query(
+        `SELECT
+        user_id
+        FROM User
+        WHERE email = ? OR user_name = ?`,
+        [userData.email, userData.user_name]
+      );
 
-        return await userModelCreateUser(userData);
-    } catch (error) {
-        throw error;
+      if (existingUser.length > 0) {
+          throw new Error('User with this email or username already exists');
+      }
+
+      const result = await connection.query(
+        `INSERT INTO User (
+          role_id,
+          department_id,
+          user_name,
+          password,
+          workstation,
+          email,
+          phone_number,
+          creation_date
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)`,
+        [
+          userData.role_id,
+          userData.department_id,
+          userData.user_name,
+          userData.password,
+          userData.workstation,
+          userData.email,
+          userData.phone_number
+        ]
+      );
+
+      return {
+        user_id: result.insertId,
+        message: 'User created succesfully'
+      };
+    } finally {
+      connection.release();
     }
   },
+
   // Find applicant by ID
   async getUserList() {
     let conn;
     try {
-      conn = await pool.getConnection();
+      conn = await db.getConnection();
       const rows = await conn.query('SELECT * FROM UserFullInfo');
       return rows;
 
