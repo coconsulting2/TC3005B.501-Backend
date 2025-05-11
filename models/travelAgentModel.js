102bc35614e5cbeb601de2ca265b5b59795a5fed
Miguel Soria <a01028033@tec.mx>
2025-05-10 21:10:48 -0600

    fix: Changed query in travelAgentModel to match the new defined status in DB

--- models/travelAgentModel.js
+++ models/travelAgentModel.js
@@ -3,50 +3,50 @@ Travel Agent Model
 Miguel Soria 26/04/25
 Queries to the DB related to travel agent actions
 */
-import pool from '../database/config/db.js';
+import pool from "../database/config/db.js";
 
 const TravelAgent = {
-  // Update request status to 4 (Atencion agencia de viaje, might be changed to 5)
-  async attendTravelRequest(requestId) {
-    let conn;
-    try {
-      conn = await pool.getConnection();
-      const result = await conn.query(
-        'UPDATE `Request` SET request_status_id = 4 WHERE request_id = ?',
-        [requestId]
-      );
-      
-      return result.affectedRows > 0;
-    } catch (error) {
-      console.error('Error updating travel request status:', error);
-      throw error;
-    } finally {
-      if (conn) {
-        conn.release();
-      }
-    }
-  },
-  
-  // Check if request exists in the DB, will be used in the model before the update
-  async requestExists(requestId) {
-    let conn;
-    try {
-      conn = await pool.getConnection();
-      const rows = await conn.query(
-        'SELECT request_id FROM `Request` WHERE request_id = ?',
-        [requestId]
-      );
-      
-      return rows.length > 0;
-    } catch (error) {
-      console.error('Error checking if request exists:', error);
-      throw error;
-    } finally {
-      if (conn) {
-        conn.release();
-      }
-    }
-  }
+    // Update request status to 4 (Atencion agencia de viaje, might be changed to 5)
+    async attendTravelRequest(requestId) {
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            const result = await conn.query(
+                "UPDATE `Request` SET request_status_id = 6 WHERE request_id = ?",
+                [requestId],
+            );
+
+            return result.affectedRows > 0;
+        } catch (error) {
+            console.error("Error updating travel request status:", error);
+            throw error;
+        } finally {
+            if (conn) {
+                conn.release();
+            }
+        }
+    },
+
+    // Check if request exists in the DB, will be used in the model before the update
+    async requestExists(requestId) {
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            const rows = await conn.query(
+                "SELECT request_id FROM `Request` WHERE request_id = ?",
+                [requestId],
+            );
+
+            return rows.length > 0;
+        } catch (error) {
+            console.error("Error checking if request exists:", error);
+            throw error;
+        } finally {
+            if (conn) {
+                conn.release();
+            }
+        }
+    },
 };
 
 export default TravelAgent;
