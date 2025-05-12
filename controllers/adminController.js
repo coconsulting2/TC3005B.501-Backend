8ef08a96086e6225579f326cea03350a47897d8e
A <A01027920@tec.mx>
2025-05-12 10:04:56 -0600

    feat(api): added controller for endpoint

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,17 +1,27 @@
 /*
 Admin Controller
 */
-import Admin from "../models/adminModel";
+import parseCSV from "../services/adminService.js";
 
 const createUser = async(req, res) => {
 
 }
 
-const createMultipleUsers = async(req, res) => {
+const createMultipleUsers = async (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ error: 'No CSV file uploaded' });
+    }
 
+    const filePath = req.file.path;
+
+    try {
+        const result = await parseCSV(filePath);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json(result);
+    }
 }
 
 export default {
-    createUser,
     createMultipleUsers
 }
