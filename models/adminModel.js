b17dc8a0fc4fa9f8f07b23cdec07f6503b9f8e32
A <A01027920@tec.mx>
2025-05-12 10:10:25 -0600

    feat(api): added queries in adminModel

--- models/adminModel.js
+++ models/adminModel.js
@@ -6,7 +6,93 @@ import pool from '../database/config/db.js';
 const Admin = {
     async createUser() {
         
-    }
+    },
+
+    async createMultipleUsers(users) {
+        let conn;
+
+        const values = users.map(user => [
+            user.role_id,
+            user.department_id,
+            user.user_name,
+            user.password,
+            user.workstation,
+            user.email,
+            user.phone_number
+        ]);
+
+        const query = `INSERT INTO User (role_id, department_id, user_name, password, workstation, email, phone_number) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+
+        try {
+            conn = await pool.getConnection();
+            const result = await conn.batch(query, values);
+            return result.affectedRows;
+        } catch (error) {
+            console.error('Error getting completed requests:', error);
+            throw error;
+        } finally {
+            if (conn){
+                conn.release();
+            } 
+        }
+    },
+
+    async findRoleID(role_name) {
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            const name = await conn.query('SELECT role_id FROM Role WHERE role_name = ?', [role_name]);
+
+            if (name && name.length > 0) {
+                return name[0].role_id;
+            }
+            return null;
+        } catch (error) {
+             console.error(`Error finding role ID for '${role_name}':`, error);
+            throw error;
+        } finally {
+            if (conn) conn.release();
+        }
+    },
+
+    async findDepartmentID(department_name) {
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            const name = await conn.query('SELECT department_id FROM Department WHERE department_name = ?', [department_name]);
+
+            if (name && name.length > 0) {
+                return name[0].department_id;
+            }
+            return null;
+        } catch (error) {
+             console.error(`Error finding department ID for '${department_name}':`, error);
+            throw error;
+        } finally {
+            if (conn) conn.release();
+        }
+    },
+
+    async findUserByEmail(email) {
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            const rows = await conn.execute('SELECT user_id FROM User WHERE email = ?', [email]);
+
+            if (rows && rows.length > 0) {
+                return true;
+            } else if (rows === undefined || rows === null) {
+                 return false;
+            }
+
+            return false;
+        } catch (error) {
+            console.error('Database Error in findUserByEmail:', error);
+            throw error;
+        } finally {
+            if (conn) conn.release();
+        }
+    },
 };
 
 export default Admin;
