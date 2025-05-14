a61ac234b7487cbbc2eda2f248c7beaa74870c1c
RoSosa <a01027913@tec.mx>
2025-05-14 13:45:16 -0600

    [feat]: Added function to validate a travel request form

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,3 +1,4 @@
+import { application } from 'express';
 import { body, param, validationResult } from 'express-validator';
 
 /*
@@ -12,9 +13,34 @@ const validateUserId = [
  * This will validate the fields in the Travel Request
  * (Sosa)
  */
-const validateTravelRequest = [
+const validateTravelRequest = validationRequest => {
+  return async (req, res, next) => {
+    for (const validation of validationRequest) {
+      const result = await validation.run(req);
+      if(!result.isEmpty()){
+        return res.status(400).json({errors: result.array()});
+      }
+    }
 
-];
+    next();
+  };
+};
+
+app.post('/create-travel-request/:id', validateTravelRequest([
+  body('router_index').isInt(),
+  body('notes').isString(),
+  body('requested_fee').isString({min: 0}),
+  body('imposed_fee').isInt({min: 0}),
+  body('origin_country_name').isString(),
+  body('origin_city_name').isString(),
+  body('destination_country_name').isString(),
+  body('beginning_date').isString(),
+  body('beginning_time').isString(),
+  body('ending_date').isString(),
+  body('eding_time').isString(),
+  body('plane_needed').isBoolean(),
+  body('hotel_needed').isBoolean()
+]));
 
 /*
  * This will validate the receipts as they are created
@@ -48,6 +74,6 @@ export default {
   validateUserId,
   validateTravelRequest,
   validateExpenseReceipts,
-  validateAuthorizer
+  validateAuthorizer,
   validateInputs
 };
