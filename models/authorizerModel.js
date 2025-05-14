2b630db3b752599f7507d4b4d9179e12fcce3abd
RicardoCalvoP <a01028889@tec.mx>
2025-05-14 13:27:17 -0600

    fixed: id names to match .yml

--- models/authorizerModel.js
+++ models/authorizerModel.js
@@ -1,4 +1,4 @@
-/* 
+/*
 Authorizer Model
 */
 
@@ -6,67 +6,67 @@ import pool from '../database/config/db.js';
 
 const Authorizer = {
 
-    async getUserRole(user_id) {
-      let conn;
-      const query = `
+  async getUserRole(user_id) {
+    let conn;
+    const query = `
         SELECT role_id FROM User WHERE user_id = ?
       `;
-      try {
-        conn = await pool.getConnection();
-        const rows = await conn.query(query, [user_id]);
-        if (rows.length > 0) {
-          return rows[0].role_id;
-        } else {
-          return null;
-        }
-      } catch (error) {
-        console.error('Error getting user role:', error);
-        throw error;
-      } finally {
-        if (conn) conn.release();
+    try {
+      conn = await pool.getConnection();
+      const rows = await conn.query(query, [user_id]);
+      if (rows.length > 0) {
+        return rows[0].role_id;
+      } else {
+        return null;
       }
-    },  
+    } catch (error) {
+      console.error('Error getting user role:', error);
+      throw error;
+    } finally {
+      if (conn) conn.release();
+    }
+  },
 
-    async authorizeTravelRequest(id, status_id) {
-        let conn;
-        const query = `
+  async authorizeTravelRequest(request_id, status_id) {
+    let conn;
+    const query = `
             UPDATE Request
             SET request_status_id = ?
             WHERE request_id = ?
         `;
-        try {
-          conn = await pool.getConnection();
-          const rows = await conn.query(query, [status_id, id]);
-          return rows;
-        } catch (error) {
-          console.error('Error getting completed requests:', error);
-          throw error;
-        } finally {
-          if (conn){
-            conn.release();
-          } 
-        }
-      },
-      async declineTravelRequest(id) {
-        let conn;
-        const query = `
+    try {
+      conn = await pool.getConnection();
+      const rows = await conn.query(query, [status_id, request_id]);
+      return rows;
+    } catch (error) {
+      console.error('Error getting completed requests:', error);
+      throw error;
+    } finally {
+      if (conn) {
+        conn.release();
+      }
+    }
+  },
+  async declineTravelRequest(id) {
+    let conn;
+    const query = `
             UPDATE Request
             SET request_status_id = 10
             WHERE request_id = ?
         `;
-        try {
-            conn = await pool.getConnection();
-            const rows = await conn.query(query, [id]);
-            return true;
-        } catch (error) {
-            console.error('Error getting completed requests:', error);
-            throw error;
-        } finally {
-            if (conn){
-            conn.release();
-            } 
-        }
-        },
+    try {
+      conn = await pool.getConnection();
+      const rows = await conn.query(query, [id]);
+      return true;
+    } catch (error) {
+      console.error('Error getting completed requests:', error);
+      throw error;
+    } finally {
+      if (conn) {
+        conn.release();
+      }
+    }
+  },
 };
 
 export default Authorizer;
