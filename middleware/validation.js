1479b8915275f23f0ed0276444eecaa0957e9f90
RoSosa <a01027913@tec.mx>
2025-05-15 14:49:05 -0600

    fix: Fixed request form validation format to accomodate JSON body

--- middleware/validation.js
+++ middleware/validation.js
@@ -14,11 +14,24 @@ const validateUserId = [
  * (Sosa)
  */
 const validateTravelRequest = [
-  body(['user_id', 'request_status_id'].isNumeric().notEmpty().withMessage('Id must be a valid number')),
-  body(['requested_fee', 'imposed_fee'].isFloat({min: 0}).notEmpty().withMessage('The minimum allowed fee is 0')),
-  body(['origin_country_name', 'origin_city_name', 'destination_country_name', 'destination_city_name'].isString().notEmpty().withMessage('City and Country must not be empty')),
-  body(['beginning_date', 'beginning_time', 'endind_date', 'ending_time'].isString().notEmpty().withMessage('Start and finish dates and times must not be empty')),
-  body(['plane_needed', 'hotal_needed'].isBoolean().notEmpty().withMessage('Please select of either hotel or plane are needed'))
+  body(['user_id'].isNumeric().notEmpty().withMessage('Id must be a valid number')),
+  body(['request_status_id'].isNumeric().notEmpty().withMessage('Request Id must be a valid number')),
+
+  body(['requested_fee'].isFloat({min: 0}).notEmpty().withMessage('The minimum requested fee is 0')),
+  body(['imposed_fee'].isFloat({min: 0}).notEmpty().withMessage('The minimum imposed fee is 0')),
+  
+  body(['origin_country_name'].isString().notEmpty().withMessage('Origin country must not be left empty.')),
+  body(['origin_city_name'].isString().notEmpty().withMessage('Origin city must not be left empty.')),
+  body(['destination_country_name'].isString().notEmpty().withMessage('Destination country must not be left empty.')),
+  body(['destination_city_name'].isString().notEmpty().withMessage('Destination city must not be left empty.')),
+
+  body(['beginning_date'].isString().notEmpty().withMessage('Trip start date cannot be empty.')),
+  body(['beginning_time'].isString().notEmpty().withMessage('Trip start time cannot be empty.')),
+  body(['ending_date'].isString().notEmpty().withMessage('Trip end date cannot be empty.')),
+  body(['ending_time'].isString().notEmpty().withMessage('Trip end time cannot be empty.')),
+
+  body(['plane_needed'].isBoolean().notEmpty().withMessage('Please select if plane reservation is required or not.')),
+  body(['hotel_needed'].isBoolean().notEmpty().withMessage('Please select if hotel reservation is required or not.'))
 ]
 
 /*
