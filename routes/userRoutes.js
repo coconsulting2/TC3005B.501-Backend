09fd467658591d537164a0f3c3b67b38fc486415
luisda25 <luisdaf04@outlook.com>
2025-05-21 01:34:26 -0600

    feat: added validation to missing endpoints

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,15 @@
 import express from 'express';
 const router = express.Router();
 import * as userController from '../controllers/userController.js';
-import { validateUserId, validateInputs } from "../middleware/validation.js";
+import { validateUserId, validateInputs, validateDeptStatus } from "../middleware/validation.js";
 
 router.route("/get-user-data/:user_id")
     .get(validateUserId, validateInputs, userController.getUserData);
 
 router.route('/get-travel-request/:request_id')
-    .get(userController.getTravelRequestById);
+    .get(validateUserId, validateInputs, userController.getTravelRequestById);
 
 router.route('/get-travel-requests/:dept/:status/:n?')
-    .get(userController.getTravelRequestsByDeptStatus);
+    .get(validateDeptStatus, validateInputs, userController.getTravelRequestsByDeptStatus);
 
 export default router;
