8a399f22636ecab8634e1f8c8f4c9aa19e94102d
luisda25 <luisdaf04@outlook.com>
2025-05-21 01:55:03 -0600

    feat: add validation for dept status

--- routes/authorizerRoutes.js
+++ routes/authorizerRoutes.js
@@ -4,14 +4,14 @@ Authorizer Routes
 import express from "express";
 const router = express.Router();
 import authorizerController from "../controllers/authorizerController.js";
-import { validateUserId, validateInputs } from "../middleware/validation.js";
+import { validateUserId, validateInputs, validateDeptStatus } from "../middleware/validation.js";
 
 router.use((req, res, next) => {
     next();
 });
 
 router.route("/get-alerts/:dept_id/:status_id/:n")
-    .get(authorizerController.authorizeTravelRequest);
+    .get(validateDeptStatus, validateInputs, authorizerController.getAlerts);
 
 router.route("/authorize-travel-request/:request_id/:user_id")
     .put(validateUserId, validateInputs, authorizerController.authorizeTravelRequest);
