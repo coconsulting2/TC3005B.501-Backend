d9d31994652ddfe8eaec8fab9de9a11e5731ae29
JoseGlezMtz <A01028517@tec.mx>
2025-05-20 22:04:49 -0600

    feat[RequestsDrafts]: connection of requests drafts with backend for get and delete

--- src/data/cookies.ts
+++ src/data/cookies.ts
@@ -3,7 +3,8 @@ import type { UserRole } from "@type/roles";
 const mockCookies = {
     username: "John Doe",
     id: "1",
-    role: "Applicant" as UserRole //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
+    role: "Applicant" as UserRole, //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
+    dept:"2"
 };
 
 export const getCookie = (key: keyof typeof mockCookies): string | UserRole => {
