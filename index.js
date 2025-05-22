ad937b25bf73f609e8a80749453b0ebc2ed9505f
RicardoCalvoP <122682647+RicardoCalvoP@users.noreply.github.com>
2025-05-22 14:14:04 -0600

    Update index.js
    Co-authored-by: Valentina Gonzalez <115291566+paydelimon22@users.noreply.github.com>

--- index.js
+++ index.js
@@ -44,16 +44,5 @@ const credentials = { key: privateKey, cert: certificate, ca: ca };
 console.clear();
 const httpsServer = https.createServer(credentials, app);
 httpsServer.listen(PORT, () =>
-    console.log(`
-         )         )            (   (
-   (  ( /(   (  ( /(      (     )\\ ))\\ )
-   )\\ )\\())  )\\ )\\())     )\\   (()/(()/(
- (((_|(_)\ (((_|(_)\   ((((_)(  /(_))(_))
- )\\___ ((_))\\___ ((_)   )\\ _ )\\(_))(_))
-((/ __/ _ ((/ __/ _ \\   (_)_\\(_) _ \\_ _|
- | (_| (_) | (_| (_) |   / _ \\ |  _/| |
-  \\___\\___/ \\___\\___/   /_/ \\_\\|_| |___|
-
-🚀 Server running on port ${PORT} with HTTPS
-`),
+    
 );
