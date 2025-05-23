cabe9d808e1a6986d3be45fd451da5ab9a7a422a
paydelimon22 <valentina2212.gonzalez@gmail.com>
2025-05-22 20:41:56 -0600

    feat: added receipt validation functionality

--- controllers/accountsPayableController.js
+++ controllers/accountsPayableController.js
@@ -35,7 +35,68 @@ const attendTravelRequest = async (req, res) => {
     }
 };
 
+const validateReceipt = async (req, res) => {
+    const receiptId = req.params.receipt_id;
+    const approval = req.body.approval;
+
+    if (approval !== 0 && approval !== 1) {
+        return res.status(400).json({
+            error: "Invalid input (only values 0 or 1 accepted for approval)"
+        });
+    }
+
+    try {
+        // Check if receipt exists
+        const receipt = await AccountsPayable.receiptExists(receiptId);
+        if (!receipt){
+            return res.status(404).json({ error: "Receipt not found" });
+        }
+
+        //Check if the receipt was already validated
+        console.log("Receipt Validation: ", receipt.validation);
+
+        if(receipt.validation != "Pendiente"){
+            return res.status(404).json({ error: "Receipt already approved or rejected" });
+        }
+
+        // Update request status to 5
+        const updated = await AccountsPayable.validateReceipt(receiptId);
+        
+        if(!updated){
+            return res
+                .status(400)
+                .json({ error: "Failed to update travel request status" });
+        }
+        
+        if (approval == 0){
+            return res.status(200).json({
+                summary: "Receipt rejected",
+                value: {
+                    receipt_id: receiptId,
+                    new_status: "Rechazado",
+                    message: "Receipt has been rejected." 
+                }
+            });
+        }
+        else if (approval == 1){
+            return res.status(200).json({
+                summary: "Receipt approved",
+                value: {
+                    receipt_id: receiptId,
+                    new_status: "Aprobado",
+                    message: "Receipt has been approved." 
+                }
+            });
+        }
+        
+    } catch (err) {
+        console.error("Error in attendTravelRequest controller:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
 // exports for the router
 export default {
     attendTravelRequest,
+    validateReceipt
 };
