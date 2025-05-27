1c838c7a086374064efea3400546157afc8f0797
RoSosa <a01027913@tec.mx>
2025-05-27 16:54:10 -0600

    feat: Added sanitization methods for trip request form

--- middleware/validation.js
+++ middleware/validation.js
@@ -27,41 +27,41 @@ export const validateDeptStatus = [
  * (Sosa)
  */
 export const validateTravelRequest = [
-  param('id').isNumeric().withMessage("ID must be a valid number"),
+  param('id').isNumeric().withMessage("ID must be a valid number").bail(),
 
-  body('router_index').isInt({ min: 0 }).withMessage('Router index must be a valid number'),
-  body('notes').isString().trim().trim().withMessage('Notes have to be a string'),
+  body('router_index').isInt({ min: 0 }).withMessage('Router index must be a valid number').bail(),
+  body('notes').isString().trim().escape().stripLow().withMessage('Notes have to be a string').bail(),
 
-  body('requested_fee').isFloat({min: 0}).exists().withMessage('The minimum requested fee is 0'),
-  body('imposed_fee').isFloat({min: 0}).exists().withMessage('The minimum imposed fee is 0'),
+  body('requested_fee').isFloat({min: 0}).exists().withMessage('The minimum requested fee is 0').bail(),
+  body('imposed_fee').isFloat({min: 0}).exists().withMessage('The minimum imposed fee is 0').bail(),
   
-  body('origin_country_name').isString().trim().notEmpty().withMessage('Origin country cannot be empty.'),
-  body('origin_city_name').isString().trim().notEmpty().withMessage('Origin city cannot be left empty.'),
-  body('destination_country_name').isString().trim().notEmpty().withMessage('Destination country cannot be left empty.'),
-  body('destination_city_name').isString().trim().notEmpty().withMessage('Destination city cannot be left empty.'),
+  body('origin_country_name').isString().trim().notEmpty().withMessage('Origin country cannot be empty.').bail(),
+  body('origin_city_name').isString().trim().notEmpty().withMessage('Origin city cannot be left empty.').bail(),
+  body('destination_country_name').isString().trim().notEmpty().withMessage('Destination country cannot be left empty.').bail(),
+  body('destination_city_name').isString().trim().notEmpty().withMessage('Destination city cannot be left empty.').bail(),
 
-  body('beginning_date').isString().trim().notEmpty().withMessage('Trip start date cannot be empty.'),
-  body('beginning_time').isString().trim().notEmpty().withMessage('Trip start time cannot be empty.'),
-  body('ending_date').isString().trim().notEmpty().withMessage('Trip end date cannot be empty.'),
-  body('ending_time').isString().trim().notEmpty().withMessage('Trip end time cannot be empty.'),
+  body('beginning_date').isString().trim().notEmpty().toDate().withMessage('Trip start date cannot be empty.').bail(),
+  body('beginning_time').isString().trim().notEmpty().withMessage('Trip start time cannot be empty.').bail(),
+  body('ending_date').isString().trim().notEmpty().toDate().withMessage('Trip end date cannot be empty.').bail(),
+  body('ending_time').isString().trim().notEmpty().withMessage('Trip end time cannot be empty.').bail(),
 
-  body('plane_needed').isBoolean().exists().withMessage('Please select if plane reservation is required or not.'),
-  body('hotel_needed').isBoolean().exists().withMessage('Please select if hotel reservation is required or not.'),
+  body('plane_needed').toBoolean().isBoolean().exists().withMessage('Please select if plane reservation is required or not.').bail(),
+  body('hotel_needed').toBoolean().isBoolean().exists().withMessage('Please select if hotel reservation is required or not.').bail(),
 
-  body('additionalRoutes').optional().isArray().withMessage('Additional routes must be an array'),
-  body('additionalRoutes.*.router_index').isNumeric().exists().withMessage("Router index must be a valid number"),
-  body('additionalRoutes.*.origin_country_name').isString().trim().notEmpty().withMessage('Origin country cannot be empty.'),
-  body('additionalRoutes.*.origin_city_name').isString().trim().notEmpty().withMessage('Origin city cannot be left empty.'),
-  body('additionalRoutes.*.destination_country_name').isString().trim().notEmpty().withMessage('Destination country cannot be left empty.'),
-  body('additionalRoutes.*.destination_city_name').isString().trim().notEmpty().withMessage('Destination city cannot be left empty.'),
+  body('additionalRoutes').optional().isArray().withMessage('Additional routes must be an array').bail(),
+  body('additionalRoutes.*.router_index').isNumeric().exists().withMessage("Router index must be a valid number").bail(),
+  body('additionalRoutes.*.origin_country_name').isString().trim().notEmpty().withMessage('Origin country cannot be empty.').bail(),
+  body('additionalRoutes.*.origin_city_name').isString().trim().notEmpty().withMessage('Origin city cannot be left empty.').bail(),
+  body('additionalRoutes.*.destination_country_name').isString().trim().notEmpty().withMessage('Destination country cannot be left empty.').bail(),
+  body('additionalRoutes.*.destination_city_name').isString().trim().notEmpty().withMessage('Destination city cannot be left empty.').bail(),
 
-  body('additionalRoutes.*.beginning_date').isString().trim().notEmpty().withMessage('Trip start date cannot be empty.'),
-  body('additionalRoutes.*.beginning_time').isString().trim().notEmpty().withMessage('Trip start time cannot be empty.'),
-  body('additionalRoutes.*.ending_date').isString().trim().notEmpty().withMessage('Trip end date cannot be empty.'),
-  body('additionalRoutes.*.ending_time').isString().trim().notEmpty().withMessage('Trip end time cannot be empty.'),
+  body('additionalRoutes.*.beginning_date').isString().trim().notEmpty().toDate().withMessage('Trip start date cannot be empty.').bail(),
+  body('additionalRoutes.*.beginning_time').isString().trim().notEmpty().withMessage('Trip start time cannot be empty.').bail(),
+  body('additionalRoutes.*.ending_date').isString().trim().notEmpty().toDate().withMessage('Trip end date cannot be empty.').bail(),
+  body('additionalRoutes.*.ending_time').isString().trim().notEmpty().withMessage('Trip end time cannot be empty.').bail(),
 
-  body('additionalRoutes.*.plane_needed').isBoolean().exists().withMessage('Please select if plane reservation is required or not.'),
-  body('additionalRoutes.*.hotel_needed').isBoolean().exists().withMessage('Please select if hotel reservation is required or not.'),
+  body('additionalRoutes.*.plane_needed').toBoolean().isBoolean().exists().withMessage('Please select if plane reservation is required or not.').bail(),
+  body('additionalRoutes.*.hotel_needed').toBoolean().isBoolean().exists().withMessage('Please select if hotel reservation is required or not.').bail(),
 ];
 
 /*
