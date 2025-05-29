57ad951ad1dffc99ba7a347d2011654a6097c29e
A <A01027920@tec.mx>
2025-05-29 13:35:10 -0600

    feat(api): added controller to update user

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -68,6 +68,75 @@ export const createUser = async (req, res) => {
     }
 }
 
+export const putUser = async (req, res) => {
+    try {
+        const userId = parseInt(req.params.user_id);
+
+        if (isNaN(userId)) {
+          console.log('Invalid user ID format');
+          return res.status(400).json({ error: 'Invalid user ID format' });
+        }
+
+        const userData = await User.getUserData(userId);
+
+        if (!userData) {
+          console.log('No user found for ID:', userId);
+          return res.status(404).json({ error: 'No information found for the user' });
+        }
+
+        const newUserData = req.body;
+        const isEmail = await Admin.findUserByEmail(req.body.email);
+        if (isEmail) {
+            return res.status(400).json({error: "Email already in use"});
+        }
+        const updatedFields = [];
+        const fieldsToUpdateInDb = {};
+        const keysToCompare = [
+            'role_name',
+            'department_name',
+            'user_name',
+            'workstation',
+            //'password',
+            'email',
+            'phone_number',
+        ];
+
+        for (const key of keysToCompare) {
+            if (newUserData[key] !== undefined && newUserData[key] !== userData[key]) {
+                updatedFields.push(key);
+                if (key === 'role_name') {
+                    const roleID = await Admin.findRoleID(newUserData[key]);
+                    if (roleID !== null) {
+                        fieldsToUpdateInDb.role_id = roleID;
+                    } else {
+                         return res.status(400).json({ error: `Invalid role name provided: ${newUserData[key]}` });
+                    }
+                } else if (key === 'department_name') {
+                    const deptId = await Admin.findDepartmentID(newUserData[key]);
+                     if (deptId !== null) {
+                         fieldsToUpdateInDb.department_id = deptId;
+                     } else {
+                          return res.status(400).json({ error: `Invalid department name provided: ${newUserData[key]}` });
+                     }
+                } else {
+                    fieldsToUpdateInDb[key] = newUserData[key];
+                }
+            }
+        }
+
+        if (Object.keys(fieldsToUpdateInDb).length > 0) {
+            await Admin.updateUser(userId, fieldsToUpdateInDb);
+
+            return res.status(200).json({ message: "User updated successfully", updated_fields: updatedFields });
+        } else {
+            return res.status(200).json({ message: "No changes detected, user data is up to date" });
+        }
+    } catch (error) {
+        console.error('An error occurred in putUser:', error);
+        return res.status(500).json({description: "Internal server error"});
+    }
+}
+
 export const deactivateUser = async (req, res) => {
     try {
         /* This doesn't work currently because there's no login yet
