5e9999e3a22c7108ef7636d6635f19b03c1f396e
luisda25 <luisdaf04@outlook.com>
2025-06-01 15:12:04 -0600

    feat: add rate limiter to authorize and decline requests

--- routes/authorizerRoutes.js
+++ routes/authorizerRoutes.js
@@ -5,6 +5,7 @@ import express from "express";
 const router = express.Router();
 import authorizerController from "../controllers/authorizerController.js";
 import { validateId, validateInputs, validateDeptStatus } from "../middleware/validation.js";
+import { generalRateLimiter } from "../middleware/rateLimiters.js";
 
 router.use((req, res, next) => {
     next();
@@ -14,9 +15,9 @@ router.route("/get-alerts/:dept_id/:status_id/:n")
     .get(validateDeptStatus, validateInputs, authorizerController.getAlerts);
 
 router.route("/authorize-travel-request/:request_id/:user_id")
-    .put(validateId, validateInputs, authorizerController.authorizeTravelRequest);
+    .put(validateId, validateInputs, generalRateLimiter, authorizerController.authorizeTravelRequest);
 
 router.route("/decline-travel-request/:request_id/:user_id")
-    .put(validateId, validateInputs, authorizerController.declineTravelRequest);
+    .put(validateId, validateInputs, generalRateLimiter, authorizerController.declineTravelRequest);
 
 export default router;
