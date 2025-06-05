7bd96cc81dbbd4a0fa24d05ae1e5c5b1c31b2ff6
luisda25 <luisdaf04@outlook.com>
2025-06-05 13:47:42 -0600

    feat: add randomizer to Initialization Vector for data encryption

--- middleware/decryption.js
+++ middleware/decryption.js
@@ -1,6 +1,5 @@
 import crypto from 'crypto';
 const AES_SECRET_KEY = process.env.AES_SECRET_KEY;
-const AES_IV = process.env.AES_IV;
 
 export const decrypt = (encryptedData) => {
   try {
@@ -8,8 +7,11 @@ export const decrypt = (encryptedData) => {
       return encryptedData; 
     }
 
-    const decipher = crypto. createDecipheriv('aes-256-cbc', Buffer.from(AES_SECRET_KEY), Buffer.from(AES_IV));
-    let decrypted = decipher.update(encryptedData, 'base64', 'utf8');
+    const IV = Buffer.from(encryptedData.slice(0, 32), 'hex');
+    const cipherText = encryptedData.slice(32);
+
+    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(AES_SECRET_KEY), IV);
+    let decrypted = decipher.update(cipherText, 'base64', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
   } catch (error) {
