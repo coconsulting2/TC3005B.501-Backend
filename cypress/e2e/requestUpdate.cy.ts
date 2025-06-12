4f61463ddb0deae9517a4a5625a9b7b56b10c2b8
RoSosa <a01027913@tec.mx>
2025-06-11 20:49:41 -0600

    feat: added cypress test to update a request as an applicant

---
+++ cypress/e2e/requestUpdate.cy.ts
@@ -0,0 +1,27 @@
+describe('Actualizar solicitud', () =>{
+  beforeEach(() => {
+    cy.visit('https://localhost:4321');
+
+    cy.get('input[placeholder*="Usuario"]').type('test1');
+    cy.get('input[placeholder*="Contraseña"]').type('test{enter}');
+
+    cy.on('window:alert', (text)=> {
+      expect(text).to.contain('Inicio de sesión exitoso');
+    });
+    cy.on('window.confirm', ()=> true);
+
+    cy.url().should('include', 'dashboard');
+  });
+
+  it('Entrar a la solicitud y actualizarla', () => {
+    cy.get('button:enabled').contains('EDITAR').click();
+
+    cy.get('input[placeholder*= "Ciudad Origen"]').clear().type('Monterrey');
+    cy.get('input[placeholder*= "Ciudad Destino"]').clear().type('Chicago');
+
+    cy.get('textarea[name=notes').clear().type('Esta solicitud fue modificada por una prueba de Cypress :D');
+
+    cy.contains('Actualizar Solicitud').should('exist').click();
+    cy.url().should('eq', 'https://localhost:4321/dashboard')
+    })
+})
