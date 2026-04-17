7ec81544d9e5d76579617fed8725db9ea0172676
Angel Montemayor Davila <angel.md@icloud.com>
2026-04-16 19:05:04 -0600

    Move ber (banxico exchage rate) test to dedicated directory

--- tests/services/BER/exchangeRate.test.js
+++ tests/services/BER/exchangeRate.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach, jest } from "@jest/globals";
-import exchangeRateService from "../services/exchangeRateService.js";
+import exchangeRateService from "../../../services/exchangeRateService.js";
 
 describe("ExchangeRateService", () => {
   beforeEach(() => {
